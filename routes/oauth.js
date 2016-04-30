71022fc5341605f059c4706b84073e73295c1e65
tmunzer <tmunzer@aerohive.com>
2016-04-30 15:25:39 +0200

    0.1.9

--- routes/oauth.js
+++ routes/oauth.js
@@ -12,8 +12,7 @@ router.get('/reg', function (req, res) {
         console.log("2");
         var authCode = req.query.authCode;
         OAuth.getPermanentToken(authCode, ApiConf.redirectUrl, ApiConf.secret, ApiConf.clientId, function(data){
-            console.log(data.error);
-            console.log(typeof data);
+            console.log(data);
             if (data.hasOwnProperty("error")) Error.render(data.error, "conf", req, res);
 
         });
