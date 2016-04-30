a2495799b7b0210430aeee60b9d89befc272aee9
tmunzer <tmunzer@aerohive.com>
2016-04-30 15:35:56 +0200

    0.1.9

--- routes/oauth.js
+++ routes/oauth.js
@@ -14,9 +14,9 @@ router.get('/reg', function (req, res) {
             else if (data.hasOwnProperty("data")) {
                 for (var owner in data.data) {
                     console.log(owner);
-                    req.session.ownerId = data[owner].ownerId;
-                    req.session.vpcUrl = data[owner].vpcUrl;
-                    req.session.accessToken = data[owner].accessToken;
+                    req.session.ownerId = data.data[owner].ownerId;
+                    req.session.vpcUrl = data.data[owner].vpcUrl;
+                    req.session.accessToken = data.data[owner].accessToken;
                 }
                 res.redirect('/web-app/');
             }
