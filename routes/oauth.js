d46c11aee6d9aedcbbbf4dd91cab2cc09016db15
tmunzer <tmunzer@aerohive.com>
2016-04-30 15:15:49 +0200

    0.1.9
    OAuth

--- routes/oauth.js
+++ routes/oauth.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var OAuth = require(appRoot + "/bin/aerohive/api/oauth");
 var ApiConf = require(appRoot + "/bin/aerohive/config");
+var Error = require(appRoot + '/routes/error');
 
 router.get('/reg', function (req, res) {
     if (req.query.hasOwnProperty('error')) {
@@ -10,8 +11,10 @@ router.get('/reg', function (req, res) {
     } else if (req.query.hasOwnProperty("authCode")) {
         console.log("2");
         var authCode = req.query.authCode;
-        OAuth.getPermanentToken(authCode, ApiConf.redirectUrl, ApiConf.secret, ApiConf.clientId, function(res){
-            console.log(res);
+        OAuth.getPermanentToken(authCode, ApiConf.redirectUrl, ApiConf.secret, ApiConf.clientId, function(data){
+            console.log(data);
+            if (data.error) Error.render(data.error, "conf", req, res);
+            
         });
     } else console.log("3");
 });
