5c2176aadf1f5371ac3fccfbb1493dbfb0c1e9c6
tmunzer <tmunzer@aerohive.com>
2016-05-07 19:32:06 +0200

    0.3.4
    correct typo

--- public/web-app/credentials/controller.js
+++ public/web-app/credentials/controller.js
@@ -45,7 +45,7 @@ angular.module('Credentials').controller("CredentialsCtrl", function ($scope, $m
             });
         }
     });
-    
+
     $scope.$watch("userGroups", function () {
         $scope.refresh();
         $scope.userGroupsLoaded = function () {
