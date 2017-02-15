22cb3143fe41e834d6fd3c0a5e05c3a1ee001000
Thomas Munzer <thomas@mbp-tmunzer.local>
2017-02-15 09:02:05 +0100

    validation fix

--- public/web-app/create/controller.js
+++ public/web-app/create/controller.js
@@ -112,7 +112,7 @@ angular.module('Create').controller("CreateCtrl", function ($scope, $rootScope, 
                 if ($scope.user.deliverMethod == "EMAIL") return $scope.user.email == "";
                 if ($scope.user.deliverMethod == "SMS") return $scope.user.phone == "";
                 if ($scope.user.deliverMethod == "EMAIL_AND_SMS") return ($scope.user.email == "" && $scope.user.phone == "");
-                else return ($scope.user.email == "" && $scope.user.phone == "");
+                else return ($scope.user.email == "" && $scope.user.phone == "" && $scope.user.name == "" && $scope.user.firstName == "" && $scope.user.lastName == "");
             }
             else if (creationType === "bulk") {
                 return (
